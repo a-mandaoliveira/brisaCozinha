4a40d239a26b096033c4fa814fecc137e71e019a
amanda.admoliveira1@gmail.com <amanda.admoliveira1@gmail.com>
2025-05-28 21:54:21 -0300

    Criação do MesaDAO.java

---
+++ src/java/DAO/MesaDAO.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Mesa;
+import util.ConectaBanco;
+
+/**
+ *
+ * @author PTOLEDO
+ */
+public class MesaDAO {
+
+    public void cadastrarMesa(Mesa m) throws ClassNotFoundException, SQLException {
+        Connection con = ConectaBanco.getConexao();
+        PreparedStatement comando = con.prepareStatement("insert into mesa (idMesa,numeroMesa,qntdLugar,valorReserva,status) values (?,?,?,?,?)");
+        comando.setInt(1, m.getIdMesa());
+        comando.setInt(2, m.getNumeroMesa());
+        comando.setInt(3, m.getQntdLugar());
+        comando.setDouble(4, m.getValorReserva());
+        comando.setString(5, m.getStatus());
+        comando.execute();
+        con.close();
+    }
+    
+    public void deletar(Mesa m) throws ClassNotFoundException, SQLException {
+        Connection con = ConectaBanco.getConexao();
+        PreparedStatement comando = con.prepareStatement("delete from mesa where id = ?");
+        comando.setInt(1, m.getIdMesa());
+        comando.execute();
+        con.close();
+    }
+    
+    public void atualizar(Mesa m) throws ClassNotFoundException, SQLException {
+        Connection con = ConectaBanco.getConexao();
+        PreparedStatement comando = con.prepareStatement("update mesa set numeroMesa = ?, qntdLugar = ?, valorReserva = ?, status = ? where id = ?");
+        comando.setInt(1, m.getNumeroMesa());
+        comando.setInt(2, m.getQntdLugar());
+        comando.setDouble(3, m.getValorReserva());
+        comando.setString(4, m.getStatus());
+        comando.setInt(5, m.getIdMesa());
+        comando.execute();
+        con.close();
+    }
+
+    public Mesa consultarById(Mesa m) throws ClassNotFoundException, SQLException {
+        Connection con = ConectaBanco.getConexao();
+        PreparedStatement comando = con.prepareStatement("select * from mesa where id = ?");
+        comando.setInt(1, m.getIdMesa());
+        ResultSet rs = comando.executeQuery();
+        Mesa mesa = new Mesa();
+        if (rs.next()){
+            mesa.setIdMesa(rs.getInt("idMesa"));
+            mesa.setNumeroMesa(rs.getInt("numeroMesa"));
+            mesa.setQntdLugar(rs.getInt("qntdLugar"));
+            mesa.setValorReserva(rs.getDouble("valorReserva"));
+            mesa.setStatus(rs.getString("status"));
+        }        
+        return mesa;
+    }
+    
+    
+    public List<Mesa> consultarTodos() throws ClassNotFoundException, SQLException {
+        Connection con = ConectaBanco.getConexao();
+        PreparedStatement comando = con.prepareStatement("select * from mesa");
+        ResultSet rs = comando.executeQuery();
+        
+        List<Mesa> lmesa = new ArrayList<Mesa>();
+        while(rs.next()){
+            Mesa mesa = new Mesa();
+            mesa.setIdMesa(rs.getInt("idMesa"));
+            mesa.setNumeroMesa(rs.getInt("numeroMesa"));
+            mesa.setQntdLugar(rs.getInt("qntdLugar"));
+            mesa.setValorReserva(rs.getDouble("valorReserva"));
+            mesa.setStatus(rs.getString("status"));
+            lmesa.add(mesa);
+        }        
+        return lmesa;
+    }
+}
